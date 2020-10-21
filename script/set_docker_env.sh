8d2a6bc0cc8ec8ba9820af0508d8601e8a1eec8e
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-10-21 16:24:28 -0500

    cleaning docker setup file

--- script/set_docker_env.sh
+++ script/set_docker_env.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo $0
 SCRIPTPATH=`dirname $(dirname $(realpath $0))`
-echo $SCRIPTPATH
-docker run -v "$SCRIPTPATH:/Trelis-plugin" -v "$2:/Trelis-sdk" -it $1 bash -c "ls /Trelis-plugin/script"
 docker run -v "$SCRIPTPATH:/Trelis-plugin" -v "$2:/Trelis-sdk" -it $1 bash -c "/Trelis-plugin/script/build_plugin.sh $3"
