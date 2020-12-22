22b9c9b1905166d917633791e98087dd1da14f8c
mouginot <mouginot@mouginots-MacBook-Pro.local>
2020-12-22 08:13:26 -0800

    update

--- script/build_plugin_macos.sh
+++ script/build_plugin_macos.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-curl https://distfiles.macports.org/MacPorts/MacPorts-2.6.3-10.15-Catalina.pkg --output MacPorts-2.6.3-10.15-Catalina.pkg
+#curl https://distfiles.macports.org/MacPorts/MacPorts-2.6.3-10.15-Catalina.pkg --output MacPorts-2.6.3-10.15-Catalina.pkg
 
 
-sudo installer -pkg MacPorts-2.6.3-10.15-Catalina.pkg -target /
+#sudo installer -pkg MacPorts-2.6.3-10.15-Catalina.pkg -target /
 
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 export MANPATH=/opt/local/share/man:$MANPATH
 export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
 
 
-sudo port selfupdate
-sudo port install autogen autoconf libtool eigen3 hdf5 patchelf cmake gcc6 wget realpath
+#sudo port selfupdate
+#sudo port install autogen autoconf libtool eigen3 hdf5 patchelf cmake gcc6 wget realpath
 
-wget https://github.com/fxcoudert/gfortran-for-macOS/releases/download/10.2/gfortran-10.2-Catalina.dmg
-hdiutil attach gfortran-10.2-Catalina.dmg
-sudo installer -pkg /Volumes/gfortran-10.2-Catalina/gfortran.pkg -target /
-hdiutil detach /Volumes/gfortran-10.2-Catalina
+#wget https://github.com/fxcoudert/gfortran-for-macOS/releases/download/10.2/gfortran-10.2-Catalina.dmg
+#hdiutil attach gfortran-10.2-Catalina.dmg
+#sudo installer -pkg /Volumes/gfortran-10.2-Catalina/gfortran.pkg -target /
+#hdiutil detach /Volumes/gfortran-10.2-Catalina
 
 
 
@@ -44,10 +44,9 @@ git clone https://bitbucket.org/fathomteam/moab -b Version5.1.0
 cd moab
 autoreconf -fi
 cd ../bld
-../moab/configure CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 \
-                  --disable-blaslapack \
+../moab/configure --disable-blaslapack \
                   --enable-shared \
-                  --enable-optimize \
+		  --enable-optimize \
                   --disable-debug \
                   --disable-blaslapack \
                   --with-eigen3=/opt/local/include/eigen3 \
@@ -61,50 +60,50 @@ mkdir -pv DAGMC/bld
 cd DAGMC
 git clone https://github.com/svalinn/DAGMC -b develop
 cd bld
-cmake ../DAGMC -DCMAKE_CXX_FLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 \
-               -DMOAB_DIR=${PLUGIN_ABS_PATH}/moab \
+cmake ../DAGMC -DMOAB_DIR=${PLUGIN_ABS_PATH}/moab \
                -DBUILD_UWUW=ON \
                -DBUILD_TALLY=OFF \
                -DBUILD_BUILD_OBB=OFF \
                -DBUILD_MAKE_WATERTIGHT=ON \
                -DBUILD_SHARED_LIBS=ON \
                -DBUILD_STATIC_LIBS=OFF \
-               -DCMAKE_BUILD_TYPE=Release \
+               -DBUILD_STATIC_EXE=OFF \
+	       -DCMAKE_BUILD_TYPE=Release \
                -DCMAKE_INSTALL_PREFIX=${PLUGIN_ABS_PATH}/DAGMC
 make -j`grep -c processor /proc/cpuinfo`
 make install
 
 
-# cd /Trelis-sdk 
-# dpkg -i Trelis-$1-Lin64.deb
 
- cd /Users/mouginot/SDK/
- tar -xzvf /Users/mouginot/SDK/Trelis-SDK-17.1.0-Lin64.tar.gz
- cd /opt/Trelis-16.5
- tar -xzvf /Trelis-sdk/Trelis-SDK-17.1.0-Lin64.tar.gz
-# https://uwmadison.box.com/shared/static/vr4wqavt2xlgv4cpc2fqk47yao0nm503.dmg
-
+cd /Applications/Trelis-17.1.app/Contents 
+tar -xzf /Users/mouginot/SDK/Trelis-SDK-17.1.0-Mac64.tar .
+cp -rf bin/* MacOS/
+rm -rf bin
+ln -s MacOS bin
+cd bin
+sudo cp -pv CubitExport-Release.cmake CubitExport-Release.cmake.orig
+#sudo port install gsed
+sudo gsed -i "s/\"Trelis-17.1.app\/Contents/\MacOS\"/\"bin\"/" CubitExport-Release.cmake
+cd 
 
 cd ${PLUGIN_ABS_PATH}/Trelis-plugin
 git submodule update --init
 
 cd ${PLUGIN_ABS_PATH}
+rm -rf bld
 mkdir -pv bld
 cd bld
-ls /opt/Trelis-${1::4}
-ls /opt/Trelis-*
-ls /opt
-cmake ../Trelis-plugin -DCUBIT_ROOT=/Users/mouginot/SDK/Trelis-17.1/Trelis-17.1.app/Contents/bin \
+cmake ../Trelis-plugin -DCMAKE_PREFIX_PATH=/Applications/Trelis-17.1.app/Contents/bin \
+		       -DCUBIT_ROOT=/Applications/Trelis-17.1.app/Contents/bin \
                        -DDAGMC_DIR=${PLUGIN_ABS_PATH}/DAGMC \
-                       -DCMAKE_BUILD_TYPE=Release \
-                       -DCMAKE_INSTALL_PREFIX=${PLUGIN_ABS_PATH}
-make -j`grep -c processor /proc/cpuinfo`
+		       -DCMAKE_INSTALL_PREFIX=${PLUGIN_ABS_PATH}
+make
 make install
-echo " cmake ../Trelis-plugin -DCubit_DIR=/opt/Trelis-${1::4} \
-                       -DCUBIT_ROOT=/opt/Trelis-${1::4} \
-                       -DDAGMC_DIR=${PLUGIN_ABS_PATH}/DAGMC \
-                       -DCMAKE_BUILD_TYPE=Release \
-                       -DCMAKE_INSTALL_PREFIX=${PLUGIN_ABS_PATH} "
+
+
+#                       -DCMAKE_BUILD_TYPE=Release \
+
+
 
 cd ${PLUGIN_ABS_PATH}
 mkdir -p pack/bin/plugins/svalinn
@@ -112,27 +111,29 @@ cd pack/bin/plugins/svalinn
 
 # Copy all needed libraries into current directory
 cp -pPv ${PLUGIN_ABS_PATH}/lib/* .
-cp -pPv ${PLUGIN_ABS_PATH}/moab/lib/libMOAB.so* .
-cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libdagmc.so* .
-cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libmakeWatertight.so* .
-cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libpyne_dagmc.so .
-cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libuwuw.so .
-cp -pPv /usr/lib/x86_64-linux-gnu/libhdf5_serial.so.100* .
+cp -pPv ${PLUGIN_ABS_PATH}/moab/lib/libMOAB.dylib .
+cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libdagmc.dylib .
+cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libmakeWatertight.dylib .
+cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libpyne_dagmc.dylib .
+cp -pPv ${PLUGIN_ABS_PATH}/DAGMC/lib/libuwuw.dylib .
+cp -pPv /opt/local/lib/libhdf5.*.dylib .
 chmod 644 *
 
 # # Set the RPATH to be the current directory for the DAGMC libraries
-# patchelf --set-rpath /opt/Trelis-${1::4}/bin/plugins/svalinn libMOAB.so
-# patchelf --set-rpath /opt/Trelis-${1::4}/bin/plugins/svalinn libdagmc.so
-# patchelf --set-rpath /opt/Trelis-${1::4}/bin/plugins/svalinn libmakeWatertight.so
-# patchelf --set-rpath /opt/Trelis-${1::4}/bin/plugins/svalinn libpyne_dagmc.so
-# patchelf --set-rpath /opt/Trelis-${1::4}/bin/plugins/svalinn libuwuw.so
+install_name_tool -change /Users/mouginot/plugin-build/moab/lib/libMOAB.0.dylib
 
-# # Create the Svalinn plugin tarball
-# cd ..
-# ln -sv svalinn/libsvalinn_plugin.so .
-# cd ../..
-# tar --sort=name -czvf svalinn-plugin_linux_$1.tgz bin
-# mv -v svalinn-plugin_linux_$1.tgz /Trelis-sdk
-# cd ..
+
+install_name_tool -add_rpath /Applications/Trelies-17.1.app/Contents/bin/plugins/svalinn libdagmc.dylib
+install_name_tool -add_rpath /Applications/Trelies-17.1.app/Contents/bin/plugins/svalinn libmakeWatertight.dylib
+install_name_tool -add_rpath /Applications/Trelies-17.1.app/Contents/bin/plugins/svalinn libpyne_dagmc.dylib
+install_name_tool -add_rpath /Applications/Trelies-17.1.app/Contents/bin/plugins/svalinn libuwuw.dylib
+
+# Create the Svalinn plugin tarball
+cd ..
+ln -sv svalinn/libsvalinn_plugin.so .
+cd ../..
+tar -czvf svalinn-plugin_mac.tgz bin
+mv -v svalinn-plugin_mac.tgz ~/
+cd ..
 # rm -rf pack bld DAGMC lib moab
 # rm Trelis-plugin
